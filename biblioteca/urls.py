1fc397cce9548c0e4e1493b211e331700b441c1a
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-01-17 08:58:01 +0100

    Add new CSS files and update URLs.py

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('eventos/', include('eventos.urls')),
+    
 ]
 
-
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
