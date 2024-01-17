8e29a7454ef0692f6e7ec688a5dd4647b69be23b
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-01-17 10:08:06 +0100

    Update ParticipantForm fields

--- eventos/forms.py
+++ eventos/forms.py
@@ -4,4 +4,4 @@ from .models import Participant
 class ParticipantForm(forms.ModelForm):
     class Meta:
         model = Participant
-        fields = ['first_name', 'last_name', 'age', 'address', 'resides_in_sp', 'time_confirmation', 'event']
+        fields = ['first_name', 'last_name', 'age', 'email', 'address', 'resides_in_sp', 'time_confirmation', 'event']
