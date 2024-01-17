64bad586b0781edf5bca5219d638cb5197e71d41
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-01-17 09:35:14 +0100

    Update CSS colors in main.scss

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 from app import views
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +11,5 @@ urlpatterns = [
     
 ]
 
-if not settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if not settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
