515f5ab80b6ab7959df9e72ec94717da545f0d48
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-01-17 10:09:12 +0100

    Update email host configuration

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # E-mail
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST_HOST')
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
