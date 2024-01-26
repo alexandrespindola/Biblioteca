185641d962a7742231f94ef4c6a9d3f7c02d44b1
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-01-26 19:58:54 +0100

    Fix CSS and template issues

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from .models import Book
+# from .models import Book
 
 def home(request):
     return render(request, 'app/home.html')
 
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'app/book_list.html', {'books': books})
+# def book_list(request):
+#     books = Book.objects.all()
+#     return render(request, 'app/book_list.html', {'books': books})
+
+
+
